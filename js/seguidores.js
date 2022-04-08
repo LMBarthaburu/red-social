8e335adb7ac9198af44a8e37b6c85f5b38f5e01d
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-04-08 16:46:13 -0300

    configuré tamaño de cards

--- js/seguidores.js
+++ js/seguidores.js
@@ -17,7 +17,7 @@ closeMenu=()=>{
 }
 
 const usersApi = async () => {
-  const urlApi = await fetch('https://randomuser.me/api/?results=10');
+  const urlApi = await fetch('https://randomuser.me/api/?results=9');
   const jsonUsersApi = await urlApi.json();
   return jsonUsersApi.results
 }
@@ -28,14 +28,14 @@ const dataUser = async () => {
   const cardContainer = document.getElementById('card-container')
 
   const cardSeguidores = dataUserApi.map(user => `
-  <div class="card-seguidores text-center pt-3 mx-1 col-3">
-          <img src="${user.picture.large}" class="image-perfil card-img-top" id="foto-de-perfil" alt="foto de perfil">
+  <div class="card-seguidores text-center pt-3 mx-1">
+          <img src="${user.picture.large}" class="image-perfil card-img-top rounded-circle" id="foto-de-perfil" alt="foto de perfil">
           <div class="card-body">
-            <h5 class="card-title" id="nombre-y-apellido">${user.name.first} ${user.name.last}</h5>
-            <h6 class="card-text nombre-usuario" id="nombre-usuario">${`@`}${user.login.username}</h6>
+            <h5 class="card-title fw-bold" id="nombre-y-apellido">${user.name.first} ${user.name.last}</h5>
+            <h6 class="card-text nombre-usuario fw-bold fst-italic" id="nombre-usuario">${`@`}${user.login.username}</h6>
             <h6 class="card-text nombre-usuario" id="edad">${user.dob.age}${` años`}</h6>
-            <p class="card-text nombre-usuario" id="ciudad-de-origen">${user.location.city}${`, `}${user.location.state}</p>
-            <a href="#" class="btn btn-light btn-sm">Seguir</a>
+            <p class="card-text nombre-usuario fst-italic lh-1" id="ciudad-de-origen">${user.location.city}${`, `}${user.location.state}</p>
+            <a href="#" class="boton-seguir btn btn-light btn-sm fw-bold">SEGUIR</a>
           </div>
         </div>
   `)
