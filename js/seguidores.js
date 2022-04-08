d8055eede201a1a61930d7a5fd585d18c2f8fd5f
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-04-07 21:05:47 -0300

    api cards seguidores

--- js/seguidores.js
+++ js/seguidores.js
@@ -17,9 +17,27 @@ closeMenu=()=>{
 }
 
 const usersApi = async () => {
-  const urlApi = await fetch('https://randomuser.me/api/');
+  const urlApi = await fetch('https://randomuser.me/api/?results=100');
   const jsonUsersApi = await urlApi.json();
-  console.log(jsonUsersApi.results)
+  return jsonUsersApi.results
 }
-usersApi()
 
+const dataUser = async () => {
+  const dataUserApi = await usersApi()
+  console.log(dataUserApi)
+  
+  const cardSeguidores = dataUserApi.map(user => `
+  <div class="card-seguidores text-center w-1 pt-3">
+          <img src="https://picsum.photos/200?random=1" class="image-perfil card-img-top" id="foto-de-perfil" alt="foto de perfil">
+          <div class="card-body">
+            <h5 class="card-title" id="nombre-y-apellido">Nombre y apellido</h5>
+            <h6 class="card-text nombre-usuario" id="nombre-usuario">@nombredeusuario</h6>
+            <h6 class="card-text nombre-usuario" id="edad">Edad</h6>
+            <p class="card-text nombre-usuario" id="ciudad-de-origen">Ciudad de origen</p>
+            <a href="#" class="btn btn-light btn-sm">Seguir</a>
+          </div>
+        </div>
+  `)
+}
+
+dataUser()
